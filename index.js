8fbdb63d2a93956202b12d57746e0c0893dce3c0
JDaddyUwu <90301531+JDaddyUwu@users.noreply.github.com>
2021-10-21 21:48:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const {
 } = require('@adiwajshing/baileys')
 
 /******COMIENZO DE LA ENTRADA DEL ARCHIVO******/
+const { ssstik } = require("./lib/tiktok.js")
 const { color, bgcolor } = require('./lib/color')
 const { bahasa } = require('./src/bahasa')
 const { negara } = require('./src/kodenegara')
@@ -46,7 +47,6 @@ const speed = require('performance-now')
 /******FIN DE ENTRADA DEL PAQUETE NPM******/
 
 /******COMIENZO DE LA ENTRADA JSON******/
-const ssstik =JSON.parse(fs.readFileSync('./database/src/tiktok.js'))
 const welkom = JSON.parse(fs.readFileSync('./database/json/welkom.json'))
 const nsfw = JSON.parse(fs.readFileSync('./database/json/nsfw.json'))
 const ban = JSON.parse(fs.readFileSync('./database/banned.json'))
