6823f76e1b57c03848ef6bc6b50ce216a6fb7650
JDaddyUwu <90301531+JDaddyUwu@users.noreply.github.com>
2021-10-22 00:42:31 -0500

    Update nsfwmenu.js

--- src/nsfwmenu.js
+++ src/nsfwmenu.js
@@ -6,7 +6,7 @@ const nsfwmenu = (prefix, pushname) => {
 - ${prefix}nsfwsidebobs
 - ${prefix}nsfwahegao
 - ${prefix}nsfwfeets
-
+- ${prefix}nsfwblowjob
 _El bot nesecita admin y tener activado los NSFW_\n _Digita_\n ${prefix}*nsfw 1*
 
 Algunas funciones fueron eliminas por errores en el servidor de la India
